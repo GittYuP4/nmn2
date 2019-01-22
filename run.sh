7754b2159e14da2d43b1b5c175a0a83fa0612c93
GittYuP4 <41740720+GittYuP4@users.noreply.github.com>
2019-01-22 17:55:38 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,5 +4,10 @@ export APOLLO_ROOT='https://github.com/GittYuP4/apollocaffe.git'
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$APOLLO_ROOT/build/lib
 export PYTHONPATH=$PYTHONPATH:$APOLLO_ROOT/python:$APOLLO_ROOT/python/caffe/proto
 
+cp ../apollocaffe/blob/master/Makefile.config.example
+sudo make -j8
+sudo make test -j8 && make runtest
+
+
 python main.py -c config/vqa_nmn.yml
 #python main.py -c config/geo_nmn.yml
